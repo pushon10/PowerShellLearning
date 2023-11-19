abeba2dfef06af0a8a7bc6299b8c71be5e8a5689
pushon10 <pushon10@googlemail.com>
2023-11-19 20:46:21 +0000

    Add initial Powershell project files

---
+++ For_each_loop.ps1
@@ -0,0 +1,13 @@
+﻿#Create an array of numbers
+$numbers = 1,2,3,4,5,6
+
+#Create a variable for the array to act upon
+$starting_number = 2
+
+#Make it so that each number in the numbers array is multiplied by the 
+foreach ($number in $numbers){
+$starting_number *= $number
+}
+
+#Print the result to the console
+Write-Host 'The total is ' $starting_number
---
+++ while_loop.ps1
@@ -0,0 +1,15 @@
+﻿#Creating an array to establish what the while loop can search through.
+$player =@('Rattrap', 'Dinobot', 'Rhinox')
+
+#Establishing that searching is set to true in order to look for the value Megatron
+$searching = $true
+#The while loop follows, which will print different statements depending on whether or not Megatron appears in the array
+while ($searching) {
+if ($player -contains 'Megatron' -or 'Rattrap'){
+    Write-Host 'The player has Megatron or Rattrap in their team'
+    $searching = $false
+}
+else {
+    Write-Host 'The player has not got Megatron or Rattrap in their team. Searching again...'
+}
+}
